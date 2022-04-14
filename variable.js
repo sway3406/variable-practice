84e21b8cd35918e393d370da837237122c1c98bd
Shane Way <shane.c.way2@gmail.com>
2022-04-13 20:27:52 -0600

    Add boolean variable

--- variable.js
+++ variable.js
@@ -5,3 +5,8 @@ var fullName = "My Name is " firstName + lastName "!";
 var ageToday = 34;
 var ageLastYear = 34 - 1;
 var ageNextYear = 34 + 1;
+
+var male = true;
+var dog = false;
+var goose = false;
+var human = false;
